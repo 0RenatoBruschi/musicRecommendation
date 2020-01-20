b5ee63b59012d2f81c2c1d365deb4dfcf1b2a9d8
Renato Bruschi <renatomassocato10@gmail.com>
2020-01-20 15:06:53 -0300

    Code restructured *with my own code c:*

--- musicRecommendation/Program.cs
+++ musicRecommendation/Program.cs
@@ -1,68 +1,92 @@
 ﻿using System;
-public class Sorteio
+
+namespace musicRecommendatioN
 {
-    // Esta é a função de embaralhamento que você deve implementar no seu código:
-    static Random _random = new Random();
 
-    public static void Shuffle<T>(T[] array)
+
+    class Program
     {
-        var random = _random;
-        for (int i = array.Length; i > 1; i--)
-        {
-            int j = random.Next(i);
-            T tmp = array[j];
-            array[j] = array[i - 1];
-            array[i - 1] = tmp;
-        }
-    }
-
-
-    
-
-    public static void Main()
-    {
-
-        Console.WriteLine("Este programa sugere musicas aleatorias com base no gênero que você escolheu.");
-        Console.WriteLine("Os gêneros possiveis são: Rock");
-        Console.WriteLine("Escolha seu gênero");
-
-        string gen;
-
-        gen = Console.ReadLine();
-
-        if (gen == "rock" | gen == "Rock" | gen == "ROCK")
+        static void Main(string[] args)
         {
 
-            string[] array = {
-         "Another One Bites the Dust - Queen",
+
+            // Array Rock e Array Rock Link (array com os links d
+            string[] rock = new string[]
+            { "Another One Bites the Dust - Queen",
          "When the levee breaks - Led Zeppelin",
          "Bohemian Rhapsody - Queen",
          "Another Brick In The Wall – Pink Floyd",
          "Strangelove - Depeche Mode",
          "Enjoy the silence - Depeche Mode",
          "Personal jesus - Depeche mode"
-            };
+             };
+            string[] rockL = new string[]
+             { "Another One Bites the Dust - Queen",
+         "When the levee breaks - Led Zeppelin",
+         "Bohemian Rhapsody - Queen",
+         "Another Brick In The Wall – Pink Floyd",
+         "Strangelove - Depeche Mode",
+         "Enjoy the silence - Depeche Mode",
+         "Personal jesus - Depeche mode"
+             };
 
-            Shuffle(array);
-            foreach (string value in array)
+
+            // Array Pop
+            string[] pop = new string[]
+            { "Don't Stop me now do grupo Queen",
+            "Enjoy the Silence do grupo Depeche Mode",
+        "Starway to Heaven do grupo Nirvana",
+        "Break up with your girlfriend, I'm bored da cantora Ariana Grande",
+        "7 Rings da cantora Ariana Grande"
+             };
+            string[] popL = new string[]
+            { "Don't Stop me now do grupo Queen",
+            "Enjoy the Silence do grupo Depeche Mode",
+        "Starway to Heaven do grupo Nirvana",
+        "Break up with your girlfriend, I'm bored da cantora Ariana Grande",
+        "7 Rings da cantora Ariana Grande"
+             };
+
+
+            Random rnd = new Random(DateTime.Now.Millisecond);
+
+            // Interface do Usuário 
+
+            Console.WriteLine("Este programa recomenda musicas aleatórias baseado no gênero que você escolher");
+            Console.WriteLine("Os gêneros disponíveis são: Rock, Pop");
+            Console.WriteLine("Selecione seu gênero...");
+
+            string gen;
+            bool nDone = false;
+
+            while (!nDone)
             {
+                gen = Console.ReadLine();
 
-                Console.WriteLine("Aqui está sua sugestão:");
-                Console.WriteLine(value);
 
-                break;
+                if (gen == "Rock")
+                {
+
+                    Console.WriteLine("Recomendo a música " + rock[rnd.Next(4)]);
+                    Console.WriteLine("Deixo aqui o link para a música: " + rockL[rnd.Next()]);
+                    nDone = true;
+                }
+                else if (gen == "Pop")
+                {
+                    Console.WriteLine("Recomendo a música " + pop[rnd.Next(4)]);
+                    Console.WriteLine("Deixo aqui o link para a música: " + popL[rnd.Next()]);
+                    nDone = true;
+                }
+                else
+                {
+                    Console.WriteLine("Parece que o gênero " + gen + " não está disponível no momento, desculpe...");
+                    Console.WriteLine("Por favor, digite um gênero entre: Rock, Pop");
+
+                }
 
             }
 
 
-
-
-
-
         }
-
-       
     }
 }
- 
- 
